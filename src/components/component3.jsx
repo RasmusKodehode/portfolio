7136f2420cb502de48f14509746d8a7999efa909
RasmusKodehode <rasmus.kodehode@gmail.com>
2022-12-13 14:21:19 +0100

    added API with useEffect

---
+++ src/components/component3.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const mainUrl = "https://tronalddump.io/random/quote";
+
+async function getAPI(callback) {
+    const fetchRequest = await fetch(mainUrl);
+    const fetchData = await fetchRequest.json();
+    callback(fetchData);
+}
+
+export function Component3() {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        /*async function getAPI() {
+            const fetchRequest = await fetch(mainUrl);
+            const fetchData = await fetchRequest.json();
+            setData(fetchData);*/
+        getAPI(setData)
+        }, [])
+
+    return (
+        <div>
+            <h1>Get a quote of the dumbest things Donald Trump ever said!</h1>
+            <button id="button-quote" onClick={() => getAPI(setData)}>Click for random quote</button>
+            <p>{data ? data.value : ""}</p>
+        </div>
+    )
+}
