6972350ee92823b6c966aa6a521460c2badb23fd
RasmusKodehode <rasmus.kodehode@gmail.com>
2022-12-13 14:21:59 +0100

    added API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Component1 } from "./components/component1";
 import { Component2 } from "./components/component2";
+import { Component3 } from "./components/component3";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <Component1 />
         <Component2 />
+        <Component3 />
       </header>
     </div>
   );
